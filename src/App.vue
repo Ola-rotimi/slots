707e5389fb4876f6cfc8e8e492e32a5753df8486
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2023-01-18 22:42:40 +0100

    Slots Practice

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <AppForm>
+    <template v-slot:help>
+      <p>This is some help text</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="username" />
+      <input type="password" placeholder="password" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+    <P>Dummy Text</P>
+  </AppForm>
+  <AppForm>
+    <template v-slot:help>
+      <p>Contact help text</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="name" />
+      <input type="text" placeholder="message" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </AppForm>
+</template>
+
+<script>
+import AppForm from "./components/Form.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppForm
+  }
+};
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,17 @@
+
+import { slotFlagsText } from '@vue/shared';
+
+<template>
+  <form>
+    <div class="help">
+        <slot name="help" />
+    </div>
+    <div class="fields">
+        <slot name="fields" />
+    </div>
+    <div class="buttons">
+        <slot name="buttons" />
+    </div>
+    <slot />
+  </form>
+</template>
